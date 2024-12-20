2a694422942274c0f00dffdceed848164a61d0d9
Rokas Rozga <rokasrozgadev@gmail.com>
2024-12-20 20:27:36 +0000

    Strict MIME type fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,35 +5,33 @@ import dotenv from 'dotenv'
 import cookieParser from "cookie-parser"
 import messageRoutes from "./routes/message.route.js"
 import cors from "cors"
-import {app, server} from "./lib/socket.js"
+import { app, server } from "./lib/socket.js"
 import path from "path"
 
 dotenv.config()
 
-
-app.use(express.json({limit: '10mb'}))
+app.use(express.json({ limit: '10mb' }))
 const PORT = process.env.PORT
 const __dirname = path.resolve()
 
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials: true
+  origin: "http://localhost:5173",
+  credentials: true
 }))
 app.use(cookieParser())
-
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/build")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist"))) // Ensure the correct path to the 'dist' directory
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html")) // Ensure the correct path to 'index.html'
+  })
 }
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-    connectDB()
+  console.log(`Server is running on port ${PORT}`)
+  connectDB()
 })
